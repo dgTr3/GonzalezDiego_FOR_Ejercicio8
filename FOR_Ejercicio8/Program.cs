e8e9375a47ef44d1d4f5500d299069595b23528d
dgTr3 <Edugta31@gmail.com>
2026-03-01 14:27:02 -0600

    completo

--- FOR_Ejercicio8/Program.cs
+++ FOR_Ejercicio8/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("");
+﻿Console.WriteLine("Mostrar una tabla donde las filas sean del 1 al 4 y las columnas del 1 al 4.");
+for (int i = 0; i <= 4; i++)
+{
+    for (int j = 0; j <= 4; j++)
+    {
+        Console.Write(i * j + "\t");
+    }
+    Console.WriteLine();
+}
